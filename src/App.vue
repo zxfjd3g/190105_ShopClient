5fac24a827936e25c7e34c2ef3e19dcdcf347fda
zxfjd3g <258147149@qq.com>
2019-06-04 16:52:01 +0800

    ajax

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,15 @@
 
 <script type="text/ecmascript-6">
   import FooterGuide from './components/FooterGuide/FooterGuide'
-
+  import {reqAddress} from './api'
   export default {
     name: 'App',
+
+    async mounted () {
+      const result = await reqAddress('116.36867', '40.10038')
+      console.log('result', result)
+    },
+
     components: {
       FooterGuide
     }
